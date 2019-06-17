c6c22ec5faecfa1666ef162101778e8c6ab4f863
Tyler Gurman <tylergurman@Tylers-MBP.home>
2019-06-16 21:50:09 -0400

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,24 +1,42 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
 
-console.log("User flipped a Queen");
+	} else{
+		console.log("Sorry, try again.");
+	}
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped a king");
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-  } else {
-    alert("Sorry, try again");
-  }
-
+	}
 }
+
+
+var flipCard = function(cardId){
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + cards[cardId]);
+	checkForMatch();
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+
+	} else{
+		alert("Sorry, try again.");
+	}
+
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
 
 
